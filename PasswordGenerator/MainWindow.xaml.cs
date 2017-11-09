b5da4214f764616f76221028aef13d764aef2fc2
jlw28 <jlwimsatt@gmail.com>
2017-11-08 20:05:55 -0500

    initial commit

---
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using MahApps.Metro.Controls;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
